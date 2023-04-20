4a0a1f9a45fb8b5658c80ecc64428f00ede95737
piyushmalav <piyushmalav85@gmail.com>
2023-04-20 23:18:08 +0530

    Api gate added

---
+++ api-gateway/src/main/java/com/pm/apigateway/ApigateWayApplication.java
@@ -0,0 +1,14 @@
+package com.pm.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class ApigateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigateWayApplication.class, args);
+	}
+
+}
